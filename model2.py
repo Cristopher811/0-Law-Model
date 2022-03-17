5edac55ad443bcf82597bb7c8399ec71c980b103
Cristopher <cristcerpug@gmail.com>
2022-03-17 14:21:36 -0600

    testing loops

--- model2.py
+++ model2.py
@@ -1,13 +1,24 @@
 import numpy as np
 def eqt(A, B, C, D):
-    if(A[0][3]):
-        C[0][3] = (A[0][3] + B[0][0] + A[1][3] + A[0][2])/4
-    if(B[0][0]):
-        D[0][0] = (B[0][0] + B[0][1] + B[1][0] + A[0][3])/4
-    print(C)
-    print("\n")
-    print(D)
-    print("\n")
+    for i in range(len(A)):
+        while (A != B).all():
+            if(A[0][3]):
+                C[0][3] = (A[0][3] + B[0][0] + A[1][3] + A[0][2])/4
+            if(B[0][0]):
+                D[0][0] = (B[0][0] + B[0][1] + B[1][0] + A[0][3])/4
+            print(C)
+            print("\n")
+            print(D)
+            print("\n")
+            if(C != D).all():
+                if(C[0][3]):
+                    A[0][3] = (C[0][3] + D[0][0] + C[1][3] + C[0][2])/4
+                if(D[0][0]):
+                    B[0][0] = (D[0][0] + D[0][1] + D[1][0] + C[0][3])/4
+                print(A)
+                print("\n")
+                print(B)
+                print("\n")
 
 TA = input("Temperatura del cuerpo A: ")
 TB = input("Temperatura del cuerpo B: ")
