35a87027eaa81293b0a5431b69114c932151be3f
Cristopher <cristcerpug@gmail.com>
2022-03-19 22:54:31 -0600

    0.1

--- model.py
+++ model.py
@@ -1,24 +1,34 @@
 import numpy as np
 
+def areSame(A,B):
+   for i in range(2):
+       if (A[i] != B[i]):
+           return 0
+   return 1
+
 A = np.float_([40,40])
 B = np.float_([30,30])
 C = np.float_([0,0])
 D = np.float_([0,0])
 
-while (A != B).all():
+i = 0
+while (areSame(A,B) == 0):
     i += 1
     C[1] = (A[0] + A[1] + B[0])/3
     C[0] = (A[0] + A[1])/2
     D[1] = (B[0] + B[1])/2
     D[0] = (A[1] + B[0] + B[1])/3
-    if (C != D).all():
+    print(C)
+    print(D)
+    break
+    if (areSame(C,D) == 0):
         A[1] = (C[0] + C[1] + D[0])/3
         A[0] = (C[0] + C[1])/2
         B[1] = (D[0] + D[1])/2
         B[0] = (C[1] + D[0] + D[1])/3
         print("A= ", A)
         print("B = ", B)
-    else:
+    elif (areSame(A,B) == 1):
         print("A = ", C)
         print("B =", D)
         print("\n")
--- model2.py
+++ model2.py
@@ -1,23 +1,17 @@
 import numpy as np
+
 def eqt(A, B, C, D):
-    while (A != B).all():
-        if(A[0][3]):
-            C[0][3] = (A[0][3] + B[0][0] + A[1][3] + A[0][2])/4
-        if(B[0][0]):
-            D[0][0] = (B[0][0] + B[0][1] + B[1][0] + A[0][3])/4
-        print(C)
-        print("\n")
-        print(D)
-        print("\n")
-        if(C != D).all():
-            if(C[0][3]):
-                A[0][3] = (C[0][3] + D[0][0] + C[1][3] + C[0][2])/4
-            if(D[0][0]):
-                B[0][0] = (D[0][0] + D[0][1] + D[1][0] + C[0][3])/4
-            print(A)
-            print("\n")
-            print(B)
-            print("\n")
+    while
+    if(A[0][3]):
+        C[0][3] = (A[0][3] + B[0][0] + A[1][3] + A[0][2])/4
+    if(B[0][0]):
+        D[0][0] = (B[0][0] + B[0][1] + B[1][0] + A[0][3])/4
+    if(A[1][3]):
+       C[1][3] = (A[1][3] + A[0][3] + A[1][2] + A[2][3] + B[1][0])/5
+    print(C)
+    print("\n")
+    print(D)
+    print("\n")
 
 TA = input("Temperatura del cuerpo A: ")
 TB = input("Temperatura del cuerpo B: ")
