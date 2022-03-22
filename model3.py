525d4e4907155757dc38a4b40536207bdf2126f0
Cristopher <cristcerpug@gmail.com>
2022-03-21 19:01:48 -0600

    your turn

--- model3.py
+++ model3.py
@@ -1,7 +1,7 @@
 import numpy as np
 
-TA = float(input("bla: "))
-TB = float(input("bla: "))
+TA = float(input("Ingresa la temperatura del cuerpo A: "))
+TB = float(input("Ingresa la temperatura del cuerpo B: "))
 A = np.full((4,4),TA)
 B = np.full((4,4),TB)
 EQ = (TA + TB)/2
@@ -17,7 +17,7 @@ def conseguir_vecinos(i,j,  arreglo):
             validos.append(p)
     return validos
 
-def eq(C):
+def equivalentes(C):
     tolerance = 1e-9
     for i in range(len(C)):
         for j in range(len(C[i])):
@@ -28,16 +28,16 @@ def eq(C):
     return 1
 
 n=0
-while(eq(C) == 0):
+while(equivalentes(C) == 0):
     n += 1
     for i in range(len(C)): #Recorre y almacena los vecinos en vecinos
         for j in range(len(C[i])):
             vecinos = conseguir_vecinos(i,j,C)
             suma_de_vecinos = 0
             for p in vecinos: #suma el valor de los vecinos (p[0] y p[1])
-                suma_de_vecinos += C[p[0]][p[1]]
+             suma_de_vecinos += C[p[0]][p[1]]
             prom_vecinos = (suma_de_vecinos)/len(vecinos)
             D[i][j] = prom_vecinos
     C = D.copy()
-    print(C, end='\n\n')
     print(n, end='\n\n')
+    print(C, end='\n\n')
