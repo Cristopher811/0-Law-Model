0f4f394b29ed157c2b0d823c2bc1470c7bff015d
Cristopher <cristcerpug@gmail.com>
2022-03-22 12:39:04 -0600

    end_version

--- model3.py
+++ model3.py
@@ -1,13 +1,30 @@
 import numpy as np
 
+n_1 = 0
+n_2 = 0
+n_3 = 0
+N = int(input("Ingresa la dimensión N de la matriz NxN: "))
 TA = float(input("Ingresa la temperatura del cuerpo A: "))
 TB = float(input("Ingresa la temperatura del cuerpo B: "))
-A = np.full((4,4),TA)
-B = np.full((4,4),TB)
-EQ = (TA + TB)/2
+TC = float(input("Ingresa la temperatura del cuerpo C: "))
 
-C = np.concatenate((A, B), axis = 1)
-D = C.copy()
+A = np.full((N,N),TA)
+B = np.full((N,N),TB)
+C = np.full((N,N),TC)
+
+AB = np.concatenate((A, B), axis = 1)
+AB_copia = AB.copy()
+
+BC = np.concatenate((B,C), axis = 1)
+BC_copia = BC.copy()
+
+AC = np.concatenate((A,C), axis = 1)
+AC_copia = AC.copy()
+
+print('\n\n')
+print(AB, end='\n\n')
+print(BC, end='\n\n')
+print(AC, end='\n\n')
 
 def conseguir_vecinos(i,j,  arreglo):
     puntos = [(i,j),(i+1,j),(i-1,j),(i,j+1),(i,j-1)]
@@ -17,8 +34,9 @@ def conseguir_vecinos(i,j,  arreglo):
             validos.append(p)
     return validos
 
-def equivalentes(C):
+def equivalentes(C, T1, T2):
     tolerance = 1e-9
+    EQ = (T1 + T2)/2
     for i in range(len(C)):
         for j in range(len(C[i])):
             tol_value = EQ - C[i][j]
@@ -27,17 +45,46 @@ def equivalentes(C):
                 return 0
     return 1
 
-n=0
-while(equivalentes(C) == 0):
-    n += 1
-    for i in range(len(C)): #Recorre y almacena los vecinos en vecinos
-        for j in range(len(C[i])):
-            vecinos = conseguir_vecinos(i,j,C)
+while(equivalentes(AB, TA, TB) == 0):
+    n_1 += 1
+    for i in range(len(AB)): #Recorre y almacena los vecinos en vecinos
+        for j in range(len(AB[i])):
+            vecinos = conseguir_vecinos(i,j,AB)
             suma_de_vecinos = 0
             for p in vecinos: #suma el valor de los vecinos (p[0] y p[1])
-             suma_de_vecinos += C[p[0]][p[1]]
+             suma_de_vecinos += AB[p[0]][p[1]]
             prom_vecinos = (suma_de_vecinos)/len(vecinos)
-            D[i][j] = prom_vecinos
-    C = D.copy()
-    print(n, end='\n\n')
-    print(C, end='\n\n')
+            AB_copia[i][j] = prom_vecinos
+    AB = AB_copia.copy()
+
+while(equivalentes(BC, TB, TC) == 0):
+    n_2 += 1
+    for i in range(len(BC)): #Recorre y almacena los vecinos en vecinos
+        for j in range(len(BC[i])):
+            vecinos = conseguir_vecinos(i,j,BC)
+            suma_de_vecinos = 0
+            for p in vecinos: #suma el valor de los vecinos (p[0] y p[1])
+             suma_de_vecinos += BC[p[0]][p[1]]
+            prom_vecinos = (suma_de_vecinos)/len(vecinos)
+            BC_copia[i][j] = prom_vecinos
+    BC = BC_copia.copy()
+
+while(equivalentes(AC, TA, TC) == 0):
+    n_3 += 1
+    for i in range(len(AC)): #Recorre y almacena los vecinos en vecinos
+        for j in range(len(AC[i])):
+            vecinos = conseguir_vecinos(i,j,AC)
+            suma_de_vecinos = 0
+            for p in vecinos: #suma el valor de los vecinos (p[0] y p[1])
+             suma_de_vecinos += AC[p[0]][p[1]]
+            prom_vecinos = (suma_de_vecinos)/len(vecinos)
+            AC_copia[i][j] = prom_vecinos
+    AC = AC_copia.copy()
+
+print('\n\n')
+print(AB, end='\n\n')
+print(n_1, end='\n\n')
+print(BC, end='\n\n')
+print(n_2, end='\n\n')
+print(AC, end='\n\n')
+print(n_3, end='\n\n')
