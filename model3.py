3711faffc7d5e0af93b14ff1eb447df2fe05c9b1
Mac <mac@MacBook-Air-de-Francisco.local>
2022-04-21 21:18:39 -0500

    actualización jueves

--- model3.py
+++ model3.py
@@ -8,70 +8,54 @@ def conseguir_vecinos(i,j,  arreglo):
             validos.append(p)
     return validos
 
-'''
-def equivalentes(C, T1, T2):
-    tolerance = 1e-9
-    EQ = (T1 + T2)/2
-    for i in range(len(C)):
-        for j in range(len(C[i])):
-            tol_value = EQ - C[i][j]
-            tol_value = abs(tol_value)
-            if (tol_value > tolerance):
-                return 0
-    return 1
-'''
-
-N = int(input("Ingresa la dimensión N de la matriz NxN: "))
-TA = float(input("Ingresa la temperatura del cuerpo A: "))
-TM1 = float(input("Ingresa la primer temperatura externa: "))
-TM2 = float(input("Ingresa la segunda temperatura externa:" ))
-TM3 = float(input("Ingresa la tercera temperatura externa: "))
-TM4 = float(input("Ingresa la cuarta temperatura extrena: "))
+N = int(input("Ingrese el tamaño de la matriz NxN: "))
 
 c = 0
 Z1 = np.zeros((1, N+2))
 Z2 = np.zeros((1,1))
 Z3 = np.zeros((N+1, N+2))
 
-A = np.full((N,N),TA)
-M1 = np.full((N, 1), TM1)
-M4 = np.full((N, 1), TM4)
+A = np.full((N,N), 25)
+M1 = np.full((N, 1), 1)
+M4 = np.full((N, 1), 4)
 A = np.append(M1, A, axis = 1)
 A = np.append(A, M4, axis = 1)
 A = np.append(A, Z1, axis = 0)
 A = np.append(Z1, A, axis = 0)
 
-
-M2 = np.full((1, N), TM2)
+M2 = np.full((1, N), 2)
 M2 = np.append(M2, Z2, axis = 1)
 M2 = np.append(Z2, M2, axis = 1)
 M2 = np.append(M2, Z3, axis = 0)
 
-M3 = np.full((1, N), TM3)
+M3 = np.full((1, N), 3)
 M3 = np.append(M3, Z2, axis = 1)
 M3 = np.append(Z2, M3, axis = 1)
 M3 = np.append(Z3, M3, axis = 0)
 
 S = A + M2 + M3
-S_C = S.copy()
+S_copia = S.copy()
 
+print(S_copia)
 
+for x in range (0, 100):
+  temp2 = x
+  temp3
 
-print('\n\n Cuerpo M1:\n\n ', S)
-
-while(c <= 2):
-  for i in range (N+1):
-    for j in range (N+1):
-      if((i == N+1 or i == 0) or (j == N+1 or j == 0)):
-        S_C[i][j] = S[i][j]
+while(c <= 1):
+  for i in range (N+2):
+    for j in range (N+2):
+      if(i == 0 or j == 0):
+        S_copia[i][j] = S[i][j]
+      elif(i == N+1 or j == N+1):
+        S_copia[i][j] = S[i][j]
       else:
         vecinos = conseguir_vecinos(i, j, S)
         sum_vecinos = 0
         for p in vecinos: 
           sum_vecinos += S[p[0]][p[1]]
           prom_vecinos = (sum_vecinos)/len(vecinos)
-          S_C[i][j] = prom_vecinos
-      S = S_C.copy()
+          S_copia[i][j] = prom_vecinos
+      S = S_copia.copy()
+      print(S_copia)
       c += 1
-      print(S_C)
-    
